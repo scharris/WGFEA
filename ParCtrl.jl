5d3a79c1e398804007fc1fe44445cc38f933506e
Stephen C Harris <stephen.harris@fda.hhs.gov>
2013-08-20 18:58:34 -0500

    Added another superconvergence option and renamed options for clarity.

--- ParCtrl.jl
+++ ParCtrl.jl
@@ -2,6 +2,7 @@ module ParCtrl
 export basis_vbf_vs_vbf_refs_in_par
 
 const parallel_basis_vbf_vs_vbf_vals = true
-const parallel_superconvergence_mesh_pairs = false
+const parallel_superconvergence_solve_and_project = false
+const parallel_superconvergence_project = true
 
 end
