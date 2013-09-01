f696aa6a8bb1c45d74eba5082cb3aefca1207429
Stephen C Harris <stephen.harris@fda.hhs.gov>
2013-09-01 12:12:21 -0500

    Remove unnecessary requires and imports.

--- WG.jl
+++ WG.jl
@@ -2,17 +2,13 @@ module WG
 export WGSolver, solve
 
 require("Common.jl")
-require("Poly.jl")
 require("Mesh.jl")
-require("WGrad.jl")
 require("WGBasis.jl")
 require("Proj.jl")
 require("VBF.jl")
-require("VBF_a_s.jl")
 require("WGSol.jl")
 
 using Common
-import Poly, Poly.Polynomial
 import Mesh, Mesh.AbstractMesh, Mesh.FENum, Mesh.OShapeNum, Mesh.FEFaceNum, Mesh.feface_one, Mesh.fenum
 import Proj
 import VBF, VBF.AbstractVariationalBilinearForm
